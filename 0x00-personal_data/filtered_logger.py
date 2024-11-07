b3d60a029b4aedb314cef877528829f937f2b711
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-07 14:45:44 +0100

    you

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Write a function called filter_datum"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -13,3 +14,25 @@ def filter_datum(
         message = re.sub(
                 rf'{fld}=[^{separator}]*', f"{fld}={redaction}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+                self.fields,
+                self.REDACTION,
+                record.getMessage(),
+                self.SEPARATOR
+            )
+        return super().format(record)
