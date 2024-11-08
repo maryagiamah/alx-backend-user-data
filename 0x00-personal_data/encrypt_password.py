70165b8ad55cf25362078bfe6c26e65cb9e333b9
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-08 13:57:24 +0100

    you

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Use the bcrypt package to perform the hashing"""
+import bcrypt
+
+
+def hash_password(password):
+    """returns a salted, hashed password, which is a byte string"""
+    byte_pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(byte_pwd, salt)
