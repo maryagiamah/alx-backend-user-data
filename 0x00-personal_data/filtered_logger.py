53576349f9f399345e8c2d38736a08bf237b17f9
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-08 11:56:37 +0100

    you

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,15 +36,15 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the database"""
-    
-    # Connect to the database using environment variables with defaults
-    return mysql.connector.connect(
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
+    config = {
+            'user': os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+            'password': os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+            'host': os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+            'database': os.getenv('PERSONAL_DATA_DB_NAME'),
+            'raise_on_warnings': True
+        }
+
+    return mysql.connector.connect(**config)
 
 
 class RedactingFormatter(logging.Formatter):
