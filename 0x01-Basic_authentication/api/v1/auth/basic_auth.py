a8ac4cc5b7d55253508ec2800950b7a5fccea0d8
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-11 19:36:58 +0100

    you

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,10 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """
+        returns the Base64 part of
+        the Authorization header
+        """
         if not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith('Basic '):
