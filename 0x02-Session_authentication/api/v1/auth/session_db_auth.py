6f25b740534803b939bc5b26914b5949254b4c30
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-14 23:37:57 +0100

    you

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""inherits from SessionExpAuth"""
+from datetime import datetime, timedelta
+from models.user_session import UserSession
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """SessionDBAuth"""
+
+    def create_session(self, user_id=None):
+        """stores new instance of UserSession"""
+
+        ses_id = super().create_session(user_id)
+
+        if ses_id is None:
+            return None
+
+        user = UserSession({"user_id": user_id, "session_id": ses_id})
+        user.save()
+
+        return ses_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """returns the User ID by requesting UserSession"""
+
+        users = UserSession.search({"session_id": session_id})
+
+        try:
+            users = UserSession.search({"session_id": session_id})
+            if users[0].created_at:
+                if self.session_duration <= 0:
+                    return users[0].user_id
+
+                exp_time = users[0].created_at + timedelta(
+                        seconds=self.session_duration
+                    )
+                if datetime.now() < exp_time:
+                    return user_id
+            return None
+        except Exception:
+            return None
+
+    def destroy_session(self, request=None):
+        """destroys the UserSession based on the Session ID"""
+
+        try:
+            users = UserSession.search({"session_id": session_id})
+
+            if users[0].user_id:
+                users[0].remove()
+                return True
+            return False
+        except Exception:
+            return False
