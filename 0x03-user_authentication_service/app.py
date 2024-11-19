d60f0f0ae3132da44a4dc5849a5630cc8b797fa0
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-19 10:49:29 +0100

    you

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,7 +68,7 @@ def profile():
     user = AUTH.get_user_from_session_id(session_id)
 
     if user:
-        return jsonify({"email": email}), 200
+        return jsonify({"email": user.email}), 200
     abort(403)
 
 
