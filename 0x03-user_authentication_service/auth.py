029e592e90c5496bde160b7c863117376f409c4c
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-19 11:44:27 +0100

    you

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,7 +4,7 @@ import bcrypt
 import uuid
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
