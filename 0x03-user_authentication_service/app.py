ed184398c531bb0d532471a13c0125ee006ed7b5
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-22 06:52:04 +0100

    you

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,6 +51,9 @@ def logout():
 
     session_id = request.cookies.get('session_id')
 
+    if not session_id:
+        abort(403)
+
     user = AUTH.get_user_from_session_id(session_id)
 
     if not user:
